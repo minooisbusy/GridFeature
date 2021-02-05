6b3df1839d2650a9aa1eb127c61e3e7c45a7e6dc
minoo <minwoo03d0@ipis.cau.ac.kr>
2021-02-05 20:26:32 +0900

    add settings

--- include/feature.h
+++ include/feature.h
@@ -1,12 +1,58 @@
 #ifndef FEATURE_H
 #define FEATURE_H
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include<vector>
 
 class ORBextractor
 {
 public:
-    ORBextractor(){};
-    operator(cv::Mat image);
+ORBextractor(int _nfeatures, float _scaleFactor, int _nlevels,
+         int _iniThFAST, int _minThFAST);
+    ~ORBextractor() {}
+
+    void operator()(cv::InputArray _image, cv::InputArray _mask,
+        std::vector<cv::KeyPoint>& _keypoints, cv::OutputArray _descriptors);
+
+
+        // Get methods
+        int inline GetLevels(){ return nlevels; }
+
+        float inline GetScaleFactor(){ return scaleFactor; }
+
+        std::vector<float> inline GetScaleFactors() { return mvScaleFactor; }
+
+        std::vector<float> inline GetInverseScaleFactors() { return mvInvScaleFactor; }
+
+        std::vector<float> inline GetScaleSigmaSquares() { return mvLevelSigma2; }
+
+        std::vector<float> inline GetInverseScaleSigmaSquares() { return mvInvLevelSigma2; }
+
+        std::vector<cv::Mat> mvImagePyramid;
+
+protected:
+
+    void ComputePyramid(const cv::Mat &image);
+    void ComputeKeyPointsOctTree(std::vector<std::vector<cv::KeyPoint>>& allKeypoints);
+    std::vector<cv::KeyPoint> DistributeOctTree(const std::vector<cv::KeyPoint>& vToDistributeKeys, 
+                                                const int &minX, const int &maxX, 
+                                                const int& minY, const int &maxY,
+                                                const int &nFeatures, const int &level);
+    std::vector<cv::Point> pattern;
+
+    int nfeatures;
+    float scaleFactor;
+    float nlevels;
+    int iniThFAST;
+    int minThFAST;
+
+    std::vector<int> mnFeaturesPerLevel;
+
+    std::vector<int> umax;
+
+    std::vector<float> mvScaleFactor;
+    std::vector<float> mvInvScaleFactor;
+    std::vector<float> mvLevelSigma2;
+    std::vector<float> mvInvLevelSigma2;
 };
 
 #endif
--- test/demo.cc
+++ test/demo.cc
@@ -1,64 +1,27 @@
-#include<opencv2/opencv.hpp>
+#include "feature.h"
 #include<iostream>
-#include<vector>
-#include<string>
-using namespace std;
-using namespace cv;
-
-void ComputePyramid(const Mat& image);
 
 int main(int argc, char* argv[])
 {
     if(argc<2){
-        cout<<"argument error"<<endl;
+        std::cout<<"argument error"<<std::endl;
         return 0;
     } 
-    Mat image = imread(argv[1]);//Mat::zeros(256,256,CV_8UC1);
+    cv::Mat image = cv::imread(argv[1]);//cv::Mat::zeros(256,256,CV_8UC1);
     if(image.empty())
     {
-        cout <<"empty error"<<endl;
+        std::cout <<"empty error"<<std::endl;
         return 0;
     }
-    const uint levels = 5; 
-    const float scaleFactor = 1.2f;
-    const uint EDGE_THRESHOLD = 19;
-    std::vector<float> mvScaleFactor(5, 0.0f);
-    std::vector<float> mvInvScaleFactor(5, 0.0f);
-    vector<Mat> mvImagePyramid(5,Mat());
-
-    mvInvScaleFactor[0] = 1.0f;
-    mvScaleFactor[0] = 1.0f;
-    for(int i = 1; i < levels; i++ )
-     mvScaleFactor[i] = mvScaleFactor[i-1] * scaleFactor;
-
-    for(int i = 0; i < levels; i++ )
-     mvInvScaleFactor[i] = 1.0f/mvScaleFactor[i];
-    for(int level = 0; level < levels; ++level)
-    {
-        cout << "level = " <<level <<endl;
-        float scale = mvInvScaleFactor[level]; 
-        Size sz(cvRound((float)image.cols*scale), cvRound((float)image.rows*scale));
-        Size wholeSize(sz.width + EDGE_THRESHOLD*2, sz.height + EDGE_THRESHOLD*2);
-        Mat temp(wholeSize, image.type()), masktemp;
-        mvImagePyramid[level] = temp(Rect(EDGE_THRESHOLD, EDGE_THRESHOLD, sz.width, sz.height));
-
-        if( level != 0)
-        {
-            resize(mvImagePyramid[level-1], mvImagePyramid[level],sz, 0, 0, INTER_LINEAR);
-            copyMakeBorder(mvImagePyramid[level], temp, EDGE_THRESHOLD, EDGE_THRESHOLD, EDGE_THRESHOLD, EDGE_THRESHOLD,
-                           BORDER_REFLECT_101+BORDER_ISOLATED); // BORDER_ISOLATED: DO NOT LOOK OUTSIDE
-        }
-        else
-        {
-            // Just Padding
-            copyMakeBorder(image, temp, EDGE_THRESHOLD, EDGE_THRESHOLD, EDGE_THRESHOLD, EDGE_THRESHOLD,
-                           BORDER_REFLECT_101);
-        }
-    string name = to_string(level) + "-th image";
-    imshow(name, mvImagePyramid[level]);
-    cout<< "Size["<<level<<"]="<<mvImagePyramid[level].size<<endl;
-    }
-    waitKey(0);
+    cv::FileStorage fSettings(argv[2], cv::FileStorage::READ);
+    int nFeatures = fSettings["ORBextractor.nFeatures"];
+    float scaleFactor = fSettings["ORBextractor.scaleFactor"];
+    int iniThFAST = fSettings["ORBextractor.iniThFAST"];
+    int minThFAST = fSettings["ORBextractor.minThFAST"];
+    int nLevels = fSettings["ORBextractor.nLevels"];
+    
+    ORBextractor extractor(nFeatures, scaleFactor, nLevels,
+                            iniThFAST, minThFAST);
 
 
 }
